ee24eabefcd63c9172ded8b5ee9fe2889a7cbc68
mohammad <mohammadhejri.ce@gmail.com>
2022-06-09 17:39:10 +0200

    Initial commit

---
+++ iTodo/iTodo/iTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTodoApp.swift
+//  iTodo
+//
+//  Created by Mohammad Hejri on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct iTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
