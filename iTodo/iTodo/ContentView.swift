4b7059a1e126de84873df500d3ce4e92c4ed33fc
Arya <nothatcreative5@gmail.com>
2022-06-20 04:36:30 -0700

    Task view changed

--- iTodo/iTodo/ContentView.swift
+++ iTodo/iTodo/ContentView.swift
@@ -7,7 +7,7 @@ class TodoTask {
     var creationDate : Date
     var name : String
     
-    init(dueDate : Date,name : String) {
+    init(dueDate : Date, name : String) {
         self.uniqueId = TodoTask.id
         self.dueDate = dueDate
         self.creationDate = Date()
@@ -27,6 +27,92 @@ struct GrowingButton: ButtonStyle {
             .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
     }
 }
+
+struct CheckboxStyle: ToggleStyle {
+
+    func makeBody(configuration: Self.Configuration) -> some View {
+
+        return HStack {
+            Image(systemName: configuration.isOn ? "checkmark.square" : "square")
+                .resizable()
+                .frame(width: 24, height: 24)
+                .foregroundColor(configuration.isOn ? .blue : .gray)
+                .font(.system(size: 20, weight: .regular, design: .default))
+                configuration.label
+        }
+        .onTapGesture { configuration.isOn.toggle() }
+
+    }
+}
+
+struct TimerView: View {
+    @State private var run = false
+    var date: Date
+
+    var body: some View {
+        HStack {
+            Image(systemName: "timer")
+                .foregroundColor(Color(UIColor.systemBlue))
+            if run {
+                Text(get_due_date(), style: .relative)
+            } else {
+                Text("Time's Up!")
+            }
+        }
+        .onAppear {
+            self.run = true
+        }
+    }
+
+    func get_due_date() -> Date {
+        DispatchQueue.main.asyncAfter(deadline: .now() + self.date.timeIntervalSinceNow) {
+            self.run = false
+        }
+        return self.date
+    }
+}
+
+
+class TaskView {
+    var task: TodoTask
+    @State var checked: Bool
+    
+    init(task: TodoTask) {
+        self.task = task
+        self.checked = false
+    }
+    
+    func date2string(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy.MM.dd, HH:mm:ss"
+        return formatter.string(from: date)
+    }
+
+    func getView() -> some View {
+        VStack(alignment : .leading) {
+            HStack() {
+                Spacer()
+                TimerView(date: self.task.dueDate)
+            }
+            Text("Task Name: \(self.task.name)")
+                .font(.headline)
+                .padding(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0))
+            HStack() {
+                Image(systemName: "calendar.badge.clock")
+                    .foregroundColor(Color(UIColor.systemBlue))
+                Text(date2string(date: self.task.dueDate))
+            }
+            HStack() {
+                Image(systemName: "calendar.badge.plus")
+                    .foregroundColor(Color(UIColor.systemBlue))
+                Text(date2string(date: self.task.creationDate))
+            }
+        }
+        .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
+    }
+}
+
+
 struct HomeView: View {
     @State var showSheet : Bool = false
     @Binding var tasks : [TodoTask]
@@ -37,15 +123,13 @@ struct HomeView: View {
                 Text("Todos")
                     .fontWeight(.bold)
                 ForEach(tasks, id : \.uniqueId) {item in
-                    VStack(alignment : .leading) {
-                        Text("Task Name: \(item.name)")
-                        Text("Created at : \(item.creationDate)")
-                        Text("Due date : \(item.dueDate)")
-                    }
+                    TaskView(task: item).getView()
+
                 }
                 .onDelete { offsets in
                     tasks.remove(atOffsets : offsets)
                 }
+
             }
             .navigationTitle("Home")
             .toolbar {
@@ -93,8 +177,8 @@ struct SheetView : View {
     @State private var isDescending : Bool = false
     
     var body : some View {
-        VStack(alignment : .center,spacing: 10) {
-            Toggle("Sort in Descending Order",isOn : $isDescending)
+        VStack(alignment : .center, spacing: 10) {
+            Toggle("Sort in Descending Order", isOn : $isDescending)
             .toggleStyle(CheckToggleStyle())
             .padding()
             Button("Sort by name") {
